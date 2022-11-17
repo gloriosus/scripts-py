d9a628e9aeead52c626e53c902e1913646f5ef2a
Vladislav Bondarenko <vladislav@bondarenko.page>
2022-11-17 20:32:17 +1000

    fix: provide vcredist 2015 for release builds
    Download the necessary distributables to fix a problem with missing 'api-ms-win-crt-runtime-l1-1-0.dll'. The distributables are placed into the deps folder.

--- build.ps1
+++ build.ps1
@@ -2,9 +2,11 @@ Invoke-WebRequest "https://www.python.org/ftp/python/3.8.10/python-3.8.10-embed-
 Expand-Archive "python-3.8.10-embed-win32.zip" -DestinationPath "bin"
 Remove-Item -Path "python-3.8.10-embed-win32.zip"
 
-$scripts = Get-ChildItem -Path "src/*" -Include *.py
+Invoke-WebRequest "http://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe" -OutFile (New-Item -Path "deps\vc_redist.x64.exe" -Force)
+Invoke-WebRequest "http://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x86.exe" -OutFile (New-Item -Path "deps\vc_redist.x86.exe" -Force)
 
 $WshShell = New-Object -comObject WScript.Shell
+$scripts = Get-ChildItem -Path "src/*" -Include *.py
 
 foreach ($script in $scripts)
 {
